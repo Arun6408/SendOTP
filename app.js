27e5305069ba274f6063125fae161b38d863115b
Arun6408 <122022303+Arun6408@users.noreply.github.com>
2024-11-09 14:29:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.post('/verify_otp', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
